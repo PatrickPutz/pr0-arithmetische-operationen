1ebe75e51600e583f39c70932489d7e6ffb2b51e
pputz <putz.patrick@outlook.com>
2021-09-26 13:14:22 +0200

    implemented arithmetic operations for a and b

--- src/ArithmetischeOperationen.java
+++ src/ArithmetischeOperationen.java
@@ -15,5 +15,11 @@ public class ArithmetischeOperationen {
 
         System.out.println("a = " + a);
         System.out.println("b = " + b);
+
+        System.out.println(a + " + " + b + " = " + (a + b));
+        System.out.println(a + " - " + b + " = " + (a - b));
+        System.out.println(a + " * " + b + " = " + (a * b));
+        System.out.println(a + " / " + b + " = " + (a / b));
+        System.out.println(a + " % " + b + " = " + (a % b));
     }
 }
