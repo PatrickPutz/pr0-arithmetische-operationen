d8e557cb0b1b3012b6af9252193dde17ebe35f3a
pputz <putz.patrick@outlook.com>
2021-09-26 13:20:05 +0200

    added comments for easier understanding

--- src/ArithmetischeOperationen.java
+++ src/ArithmetischeOperationen.java
@@ -2,9 +2,12 @@ import java.util.Scanner;
 
 public class ArithmetischeOperationen {
     public static void main(String[] args) {
+
+        // initialize a and b as placeholders for later
         int a = 0;
         int b = 0;
 
+        // ask for the definitions of a and b
         System.out.print("Define a: ");
         Scanner defineA = new Scanner(System.in);
         a = defineA.nextInt();
@@ -13,9 +16,11 @@ public class ArithmetischeOperationen {
         Scanner defineB = new Scanner(System.in);
         b = defineB.nextInt();
 
+        // print a and b for verification
         System.out.println("a = " + a);
         System.out.println("b = " + b);
 
+        // execute arithmetic operations for a and b
         System.out.println(a + " + " + b + " = " + (a + b));
         System.out.println(a + " - " + b + " = " + (a - b));
         System.out.println(a + " * " + b + " = " + (a * b));
